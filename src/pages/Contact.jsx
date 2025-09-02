180606b767bffa3030a8dda7164c03b68b3f9ebf
huangxiaoqian <510427625@qq.com>
2025-09-02 12:16:36 +0800

    Update contact

--- src/pages/Contact.jsx
+++ src/pages/Contact.jsx
@@ -136,7 +136,7 @@ export default function Contact() {
     }
 
     try {
-      const response = await fetch("https://formspree.io/f/xyzpwewl", {
+      const response = await fetch("https://formspree.io/f/xanbogov", {
         method: "POST",
         headers: { "Content-Type": "application/json" },
         body: JSON.stringify(formData),
